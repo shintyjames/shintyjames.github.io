3ebf0f8c521170870c4f663faf340ebe5a93e0ab
shintaro <sito@newroads.org>
2017-10-30 09:42:20 -0700

    tictactoe OX both

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,6 +1,15 @@
-var character = 'x';
+var character = 'X';
 
 //This function handles the onclick event
 function turn(location){
+	
+	//switch between X and O
+	if(character == 'X'){
+		character = 'O';
+	}
+	else{
+		character = 'X';
+	}
+	
 	document.getElementById(location).innerHTML = character;
 }
