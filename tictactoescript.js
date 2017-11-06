672027204339c4b9406060574335186004ed9a34
shintaro <sito@newroads.org>
2017-11-06 08:29:18 -0800

    tic tac toe turn

--- tictactoescript.js
+++ tictactoescript.js
@@ -3,13 +3,22 @@ var character = 'X';
 //This function handles the onclick event
 function turn(location){
 	
-	//switch between X and O
-	if(character == 'X'){
-		character = 'O';
-	}
-	else{
-		character = 'X';
+	//update box with chracter, if box is empty
+	if(document.getElementById(location).innerHTML == ""){
+	
+		//update results div with feedback
+		document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
+	
+		//switch between X and O
+		if(character == 'X'){
+			character = 'O';
+		}
+		else{
+			character = 'X';
+		}	
+	
+		//write character in box
+		document.getElementById(location).innerHTML = character;
 	}
 	
-	document.getElementById(location).innerHTML = character;
 }
