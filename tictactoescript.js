7738f96807bc64722f8e4610ec595d494e651d10
shintaro <sito@newroads.org>
2017-11-13 09:23:52 -0800

    tic tac toe vertical horizontal diagnol

--- tictactoescript.js
+++ tictactoescript.js
@@ -19,6 +19,37 @@ function turn(location){
 	
 		//write character in box
 		document.getElementById(location).innerHTML = character;
+		//check for winner after move
+		checkWinner();
 	}
+
 	
+}
+
+//check for winner
+function checkWinner(){
+	
+	//grab HTML of each tic tac toe div (which holds X or O)
+	var r1c1 = document.getElementById("r1c1").innerHTML;
+	var r1c2 = document.getElementById("r1c2").innerHTML;
+	var r1c3 = document.getElementById("r1c3").innerHTML;
+	var r2c1 = document.getElementById("r2c1").innerHTML;
+	var r2c2 = document.getElementById("r2c2").innerHTML;
+	var r2c3 = document.getElementById("r2c3").innerHTML;
+	var r3c1 = document.getElementById("r3c1").innerHTML;
+	var r3c2 = document.getElementById("r3c2").innerHTML;
+	var r3c3 = document.getElementById("r3c3").innerHTML;
+	
+	if(((r1c1 == r1c2) && (r1c1 == r1c3) && r1c1 != '') ||
+       ((r2c1 == r2c2) && (r2c1 == r1c3) && r2c1 != '') ||
+	   ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') ||
+	   ((r1c1 == r2c2) && (r1c1 == r3c1) && r1c1 != '') ||
+	   ((r1c3 == r2c2) && (r1c3 == r3c1) && r1c3 != '') ||
+	   ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') ||
+	   ((r1c2 == r2c2) && (r1c2 == r3c2) && r1c2 != '') ||
+	   ((r1c3 == r2c3) && (r1c3 == r3c3) && r1c3 != '')) {
+		
+		//update results div
+		document.getElementById("results").innerHTML = character + " wins!";
+	}
 }
